5f5c7ee34a4bb80ddce1c2de10d4a592f8625a4b
Sarah <kretaksson@gmail.com>
2024-06-08 15:28:52 -0700

    1

--- main.cc
+++ main.cc
@@ -61,7 +61,7 @@ int main()
     camera cam;
 
     cam.aspect_ratio = 16.0 / 9.0;
-    cam.image_width = 300;
+    cam.image_width = 400;
     cam.samples_per_pixel = 50;
     cam.max_depth = 10;
 
